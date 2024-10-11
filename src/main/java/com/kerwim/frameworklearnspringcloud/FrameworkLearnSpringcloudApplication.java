60a00546056b8b4eb9283782b23bfe615bed3285
mac <0924>
2024-10-11 23:34:54 +0800

    intro

---
+++ src/main/java/com/kerwim/frameworklearnspringcloud/FrameworkLearnSpringcloudApplication.java
@@ -0,0 +1,13 @@
+package com.kerwim.frameworklearnspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkLearnSpringcloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameworkLearnSpringcloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kerwim/frameworklearnspringcloud/FrameworkLearnSpringcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerwim.frameworklearnspringcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkLearnSpringcloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
